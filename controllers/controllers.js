bb5c9223d5dd582247aff3aa2cbc020de5042c66
alopezmb <alejandro.lopez.martinez@alumnos.upm.es>
2018-05-20 12:25:27 +0200

    Hopefully this works

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -2,19 +2,19 @@ const {models}=require('../models/index');
 
 /*Index Controller*/
 const indexController=(req, res, next)=> {
-    res.render('index', { title: 'Express'});
+    res.render('index', { title: 'Core Quiz 2018'});
 };
 
 /*Credits controller*/
 const creditsController=(req, res, next)=> {
 
-    res.render('credits', { author: 'Alejandro López Martínez'});
+    res.render('credits', {title: 'Credits', author: 'Alejandro López Martínez'});
 };
 
 const quizzesController=(req, res, next)=> {
 
     models.quiz.findAll().then(quizzes=>{
-        res.render('quizzes/index.ejs', { quizzes:quizzes});
+        res.render('quizzes/index.ejs', {title:'Lista de Quizzes', quizzes:quizzes});
         }) .catch(error => {
             errorlog(error.message);
 
